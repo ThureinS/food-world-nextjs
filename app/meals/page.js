70b187f5551144f8850cc89c83c35ce65d9a322a
ThureinS <94038134+ThureinS@users.noreply.github.com>
2026-03-21 23:43:43 +0630

    loading screen is now taking only the meal grid place

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,12 +2,16 @@ import Link from "next/link";
 import styles from "./page.module.css";
 import MealsGrid from "./meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-const Meals = async () => {
+const MealFetch = async () => {
   const meals = await getMeals();
 
-  {
+  return <MealsGrid meals={meals} />;
+};
 
+const Meals = () => {
+  {
   }
 
   return (
@@ -23,7 +27,11 @@ const Meals = async () => {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching Meals....</p>}
+        >
+          <MealFetch />
+        </Suspense>
       </main>
     </>
   );
